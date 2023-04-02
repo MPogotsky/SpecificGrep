d785b4ce47eeabe4e7186ea5129630147eb1bd02
Pahotski Matsvei <matsvei.pahotski@nokia.com>
2023-04-02 15:46:14 +0200

    Add the rest of options and split logic into separete functions

--- SpecificGrep.cpp
+++ SpecificGrep.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
-#include <boost/program_options.hpp>
 #include "SpecificGrep.hpp"
 
-namespace po = boost::program_options;
-
-SpecificGrep::SpecificGrep(){};
+SpecificGrep::SpecificGrep()
+:declaredOptions(declareOptions())
+{};
 
 SpecificGrep::~SpecificGrep(){};
 
-void SpecificGrep::parseArguments(int argc, char *argv[])
+options_t SpecificGrep::declareOptions()
 {
-    po::options_description desc("Options");
-    desc.add_options()
-        ("dir,d", po::value<std::string>()->default_value("."), "Start directory where program needs to look for files (also in subfolders)")
-        ("log_file,l", po::value<std::string>()->default_value("specific_grep.log"), "Name of the log file")
-        ("help", "These menu")
+    options_t specificGrepOpt;
+
+    specificGrepOpt.standard.add_options()
+        ("help",            "These menu")
+        ("dir,d",           po::value<std::string>()->default_value("."),
+                            "Start directory (subfolders aware)")
+        ("log_file,l",      po::value<std::string>()->default_value("specific_grep.log"),
+                            "Name of the log file")
+        ("result_file, r",  po::value<std::string>()->default_value("specific_grep.txt"),
+                            "Name of the file where result is given")
+        ("threads, t",      po::value<int>()->default_value(4),
+                            "Number of threads in the pool")
+        ("pattern",         po::value<std::string>(), "Search pattern")
     ;
 
-    if (argc == 1) {
-        std::cout << desc << std::endl;
-        exit(EXIT_FAILURE);
-    }
+    specificGrepOpt.positional.add("pattern", 1);
 
-    for (int i = 1; i < argc; i++) {
-        auto arg = std::string(argv[i]);
-        if (arg == "-" || arg == "--") {
-            std::cerr << "Error: invalid command line syntax" << std::endl;
-            std::cerr << "Use --help for help" << std::endl;
-            exit(EXIT_FAILURE);
-        }
+    return specificGrepOpt;
+}
+
+void SpecificGrep::printHelp() const
+{
+    std::cout << "Usage: \n" << "\t ./specific_grep PATTERN OPTIONS \n" << std::endl;
+    std::cout << declaredOptions.standard << std::endl;
+}
+
+void SpecificGrep::parseArguments(int argc, char *argv[])
+{
+    if (argc == 1) {
+        printHelp();
+        exit(EXIT_FAILURE);
     }
 
     po::variables_map vm;
     try {
-        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::command_line_parser parser(argc, argv);
+        parser.options(declaredOptions.standard);
+        parser.positional(declaredOptions.positional);
+        po::store(parser.run(), vm);
         po::notify(vm);
     } catch (po::error& e) {
         std::cerr << "Error: " << e.what() << std::endl;
@@ -42,7 +56,7 @@ void SpecificGrep::parseArguments(int argc, char *argv[])
     }
 
     if (vm.count("help")) {
-        std::cout << desc << std::endl;
+        printHelp();
         exit(EXIT_SUCCESS);
     }
 
@@ -51,9 +65,20 @@ void SpecificGrep::parseArguments(int argc, char *argv[])
     }
 
     if (vm.count("log_file")) {
-        std::cout << "Output file name: " << vm["log_file"].as<std::string>() << std::endl;
-    } else {
-        std::cout << "Output file not set.\n";
+        std::cout << "Loggin to: " << vm["log_file"].as<std::string>() << std::endl;
     }
 
+    if (vm.count("result_file")) {
+        std::cout << "Results to: " << vm["result_file"].as<std::string>() << std::endl;
+    }
+
+    if (vm.count("threads")) {
+        std::cout << "Number of threads: " << vm["threads"].as<int>() << std::endl;
+    }
+
+    if (vm.count("pattern")) {
+        std::cout << "Search for: " << vm["pattern"].as<std::string>() << std::endl;
+    }
+
+    exit(EXIT_SUCCESS);
 }
--- SpecificGrep.hpp
+++ SpecificGrep.hpp
@@ -1,5 +1,17 @@
 #pragma once
 
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+typedef struct opt
+{
+    opt():standard("Options"){};
+    po::options_description standard;
+    po::positional_options_description positional;
+} options;
+
+typedef options options_t;
 
 class SpecificGrep
 {
@@ -8,7 +20,11 @@ public:
     ~SpecificGrep();
 
     void parseArguments(int argc, char *argv[]);
+private:
+    const options_t declaredOptions;
 
+    options_t declareOptions();
+    void printHelp() const;
 };
 
 inline SpecificGrep entryPoint;
