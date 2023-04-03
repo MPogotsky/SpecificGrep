5597655958523a3f93302d42869802d9d73b182e
Pahotski Matsvei <matsvei.pahotski@nokia.com>
2023-04-03 18:04:02 +0200

    Fix Scout to use threads

--- Scout.cpp
+++ Scout.cpp
@@ -1,35 +1,44 @@
-#include <string>
-#include <vector>
-
-#include <iostream> // not necessary
-
 #include "Scout.hpp"
 
+bmux Scout::coutMutex;
+bmux Scout::filesMutex;
+bmux Scout::poolMutex;
+std::vector<std::string> Scout::findings;
+
 Scout::Scout(const std::string &directory)
-    : searchingArea(directory),
-      findings({})
+    : entryDir(directory)
 {
-    searchTheArea();
+    entryPoint();
 }
 
 Scout::~Scout(){};
 
-void Scout::searchTheArea()
+void Scout::entryPoint()
 {
-    for (auto const &dirEntry : std::filesystem::recursive_directory_iterator{searchingArea})
-    {
-        // Write down only non-empty file path`s inside findings
-        if (dirEntry.is_regular_file())
-        {
-            if (!std::filesystem::is_empty(dirEntry.path()))
-            {
-                findings.push_back(dirEntry.path());
-            }
-        }
-    }
+    asio::thread_pool pool(boost::thread::hardware_concurrency());
+    asio::post(pool, boost::bind(&Scout::searchTheArea, this, entryDir, boost::ref(pool)));
+
+    pool.wait();
 }
 
 std::vector<std::string> Scout::getFindings() const
 {
     return findings;
+}
+
+void Scout::searchTheArea(const fs::path &dir, asio::thread_pool &pool)
+{
+    for (fs::directory_entry &entry : fs::directory_iterator(dir, fs::directory_options::skip_permission_denied))
+    {
+        if (is_directory(entry.path()) && !is_empty(entry.path()))
+        {
+            lock_guard pool_lock(poolMutex);
+            post(pool, boost::bind(&Scout::searchTheArea, this, entry.path(), boost::ref(pool)));
+        }
+        else if (is_regular_file(entry.path()) && !is_empty(entry.path()))
+        {
+            lock_guard lock(filesMutex);
+            findings.push_back(entry.path().string());
+        }
+    }
 }
--- Scout.hpp
+++ Scout.hpp
@@ -1,15 +1,38 @@
 #pragma once
 
-#include <filesystem>
+#include <string>
+#include <vector>
+
+#include <boost/thread/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/asio/thread_pool.hpp>
+#include <boost/asio/post.hpp>
+
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
+namespace asio = boost::asio;
+
+using bmux = boost::mutex;
+using lock_guard = boost::lock_guard<bmux>;
+
 
 class Scout
 {
 public:
     explicit Scout(const std::string& directory);
     ~Scout();
+
     std::vector<std::string> getFindings() const;
+
 private:
-    std::vector<std::string> findings;
-    std::filesystem::path searchingArea;
-    void searchTheArea();
+    void entryPoint();
+    void searchTheArea(const fs::path &dir, asio::thread_pool &pool);
+
+    fs::path entryDir;
+    static std::vector<std::string> findings;
+
+    static bmux coutMutex;
+    static bmux filesMutex;
+    static bmux poolMutex;
 };
